4a5374be34bb0a6dbfa75c939580fac4315ae3d1
Vadim Starichkov <starichkovva@gmail.com>
2022-05-08 16:30:47 +0300

    Tests for 'Order' routing added

--- src/test/kotlin/com/templatetasks/kotlin/ktor/ApplicationTest.kt
+++ src/test/kotlin/com/templatetasks/kotlin/ktor/ApplicationTest.kt
@@ -1,18 +1,12 @@
 package com.templatetasks.kotlin.ktor
 
-import io.ktor.server.routing.*
-import io.ktor.http.*
-import io.ktor.serialization.kotlinx.json.*
-import io.ktor.server.plugins.*
-import io.ktor.server.engine.*
-import io.ktor.server.application.*
-import io.ktor.server.response.*
-import io.ktor.server.request.*
+import com.templatetasks.kotlin.ktor.plugins.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
-import kotlin.test.*
+import io.ktor.http.*
 import io.ktor.server.testing.*
-import com.templatetasks.kotlin.ktor.plugins.*
+import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class ApplicationTest {
     @Test
