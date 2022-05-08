53d93feefd7e3755060d0439e014ca8e3c52c74d
Vadim Starichkov <starichkovva@gmail.com>
2022-05-08 15:08:38 +0300

    Initial service

---
+++ src/main/kotlin/com/templatetasks/kotlin/ktor/Application.kt
@@ -0,0 +1,13 @@
+package com.templatetasks.kotlin.ktor
+
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import com.templatetasks.kotlin.ktor.plugins.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+        configureRouting()
+        configureSerialization()
+        configureAdministration()
+    }.start(wait = true)
+}
